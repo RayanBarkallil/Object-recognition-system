2582c9ec71a53e5b3904a881fb418163a8a2bb39
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2022-06-01 16:44:32 +0100

    Updates : - Added REST-API (needs expanding) ----------------------------------- TODOs : - first page - function to eliminate redundant detection rectangles (goto detect.py)

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, session,redirect, url_for
+from flask import Flask, render_template, request, session,redirect, url_for,jsonify
+from flask_restful import Api, Resource
 from prediction import getPediction
-import numpy as np
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "superdupersecretkey"
-
+UPLOAD_FOLDER = 'static/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/',methods=['GET'])
@@ -16,7 +19,7 @@ def doThings():
     #save the image -----------------------------------------------------------------------------------------
     imagefile = request.files['imagefile']
     splitting = imagefile.filename.split('.')
-    extension = splitting[len(splitting)-1]
+    extension = splitting[len(splitting)-1].lower()
     defaultFilename = "todo."+extension
     image_path ="./static/images/" + defaultFilename
     imagefile.save(image_path)
@@ -44,11 +47,31 @@ def showPredictions():
 
 
 
+#API SECTION :
+#test--------------------------------------------------------------------------------------------------
+api = Api(app)
+todos ={}
+class ObjectDetectionAPI(Resource):
+    def post(self):
+        imagefile = request.files['imagefile']
+        splitting = imagefile.filename.split('.')
+        extension = splitting[len(splitting)-1].lower()
+        defaultFilename = "todo."+extension
+        image_path ="./static/images/" + defaultFilename
+        imagefile.save(image_path)
+        #do prediction -----------------------------------------------------------------------------------------
+        cropped_img_name_list,label_list = getPediction(image_path)
+        #info dictionary for template filling ------------------------------------------------------------------
+        croppedImageList_andLabels = {}
+        for i in range(len(cropped_img_name_list)) :
+            croppedImageList_andLabels[cropped_img_name_list[i]] = {
+                "label" : label_list[i] #todo add coordinates 3la kulla rectangle
+            }
+        return croppedImageList_andLabels
 
 
+api.add_resource(ObjectDetectionAPI, "/api/detect")
 
 
-if __name__ =="__main__":
-    app.run(port=3000, debug=True)
-
-
+if __name__=="__main__":
+    app.run(host= '0.0.0.0',port=5000,debug=True)
